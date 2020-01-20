80b27fc71ac872bb36ea9e26549e4fa06dae13c2
Sascha Andeweg <sascha@designed.irish>
2020-01-20 21:55:35 +0000

    commit of the second challenge jQuery Changing HTML

---
+++ jQueryChangingHTMLChallenge2.js
@@ -0,0 +1,5 @@
+// solutions to challenge 2 of Changing HTML/CSS with jQuery
+
+$("p").append("<span>Lorem ipsum text for educational purposes</span>");
+$(".bottom_button").remove();
+$(".card").empty();
