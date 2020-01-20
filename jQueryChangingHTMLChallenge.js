c4ea76636c73b2837563da1ed69c640d6bccdf63
Sascha Andeweg <sascha@designed.irish>
2020-01-20 21:44:02 +0000

    commit of Changing HTML challenge with clarification comments

--- jQueryChangingHTMLChallenge.js
+++ jQueryChangingHTMLChallenge.js
@@ -1,4 +1,6 @@
+// playing with methods
 $("p").empty();
 $("p").text("This is the new text for all paragraphs");
 
+// solution to the challenge
 $("p").text("This is the new text for all paragraphs, but without using the method empty first");
